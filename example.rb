4bdf3948ab190245a9ce4afb4f07b7ddb359e0d3
eric feldman <ericfeldman93@gmail.com>
2014-04-10 20:50:34 +0300

    change the main file name to ruby2soap

--- example.rb
+++ example.rb
@@ -1,7 +1,7 @@
-require_relative 'lib\wcf_handler'
+require_relative 'lib\ruby2soap'
 
 def complex_object_example
-	handler = WcfHandler.new('http://localhost:1659/Service1.svc')
+	handler = Ruby2Soap.new('http://localhost:1659/Service1.svc')
 	res = handler.GetDataUsingDataContract(:composite => {
 		:BoolValue => true,
 		:StringValue => "ruby2soap"
@@ -9,24 +9,24 @@ def complex_object_example
 end
 
 def cookie_state_full_example
-	handler = WcfHandler.new('http://localhost:1659/Service1.svc')
+	handler = Ruby2Soap.new('http://localhost:1659/Service1.svc')
 	handler.Init(:userName => 'ericman93')
 	handler.IncreaseScore()
 end
 
 def few_params_example
-	handler = WcfHandler.new('http://www.webservicex.net/CurrencyConvertor.asmx')
+	handler = Ruby2Soap.new('http://www.webservicex.net/CurrencyConvertor.asmx')
 	handler.ConversionRate(:FromCurrency => 'ILS', :ToCurrency => 'GBP')
 end
 
 def ntlm_example
-	handler = WcfHandler.new('http://localhost:1659/Service1.svc')
+	handler = Ruby2Soap.new('http://localhost:1659/Service1.svc')
 	handler.AddPoints({:points => 6},SecutryProtocol::NTLM,'user','password','domain')
 end
 
 
 #puts few_params_example.body
-handler = WcfHandler.new('http://localhost:1659/Service1.svc')
+handler = Ruby2Soap.new('http://localhost:1659/Service1.svc')
 res = handler.GetDataUsingDataContract(:composite => {
 	:BoolValue => true,
 	:StringValue => "ruby2soap"
