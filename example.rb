1dca43cc1792a0922916f724aee0775940eece8b
Gustavo Canedo <gknedo@gmail.com>
2017-07-24 22:52:44 -0300

    update examples

--- example.rb
+++ example.rb
@@ -1,26 +1,26 @@
 require 'ruby2soap'
 
 def complex_object_example
-	handler = Ruby2Soap.new('http://localhost:1659/Service1.svc')
-	res = handler.GetDataUsingDataContract(:composite => {
-		:BoolValue => true,
-		:StringValue => "ruby2soap"
+	handler = Canned::Client.new('http://localhost:1659/Service1.svc')
+	res = handler.GetDataUsingDataContract(composite: {
+		BoolValue: true,
+		StringValue: "canned"
 	})
 	res.result['StringValue']
 end
 
 def cookie_state_full_example
-	handler = Ruby2Soap.new('http://localhost:1659/Service1.svc')
-	handler.Init(:userName => 'ericman93')
+	handler = Canned::Client.new('http://localhost:1659/Service1.svc')
+	handler.Init(userName: 'ericman93')
 	handler.IncreaseScore()
 end
 
 def few_params_example
 	handler = Canned::Client.new('http://www.webservicex.net/CurrencyConvertor.asmx')
-	handler.ConversionRate(:FromCurrency => 'ILS', :ToCurrency => 'GBP').result
+	handler.ConversionRate(FromCurrency: 'ILS', ToCurrency: 'GBP').result
 end
 
 def ntlm_example
-	handler = Ruby2Soap.new('http://localhost:1659/Service1.svc')
-	handler.AddPoints({:points => 6},SecutryProtocol::NTLM,'user','password','domain')
+	handler = Canned::Client.new('http://localhost:1659/Service1.svc')
+	handler.AddPoints({points: 6}, Canned::Web::SecutryProtocol::NTLM, 'user', 'password', 'domain')
 end
